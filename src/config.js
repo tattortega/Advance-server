2799a058f1f2dec8f0d786c0e74c99a85d4702be
Ricardo Ortega <tattortega.28@gmail.com>
2022-04-11 17:13:23 -0500

    configuration postgresql

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     db: {
-      user: process.env.DB_USER || "postgres",
-      password: process.env.DB_PASSWORD || "tatto",
-      localhost: process.env.DB_HOST || "localhost",
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_DATABASE || "advance",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
     },
   };
